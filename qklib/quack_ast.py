758a3e889de7b1343853599005dce9af32735197
Tong Guan <yuanyuanbingzi@gmail.com>
2024-05-13 19:22:35 -0700

    add ast nodes file

---
+++ qklib/quack_ast.py
@@ -0,0 +1,42 @@
+#AST节点类用于表示抽象语法树（AST)的结构。每个节点类对应一种语法结构。
+class Assign:
+    # Assign表示赋值语句
+    def __init__(self, var, vartype, expr):
+        self.var = var
+        self.vartype = vartype
+        self.expr = expr
+    
+    def __repr__(self):
+        return f"Assign({self.var}, {self.vartype}, {self.expr})"
+
+
+class Add:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right 
+    
+    def __repr__(self):
+        return f"Add({self.left}, {self.right})"
+
+class Sub:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right 
+    
+    def __repr__(self):
+        return f"Sub({self.left}, {self.right})"
+
+class Var:
+    #Var表示变量
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return f"Var({self.name})"
+
+class Number:
+    #Number表示整数常量
+    def __init__(self, value):
+        self.value = value
+    
+    def __repr__(self):
+        return f"Number({self.value})"
